78c94b43f42b58aca9eeedf29cfc786083b5f523
Andrei Svirski <33blogas@gmail.com>
2024-08-20 23:00:36 +0200

    react: auth api request

--- README.md
+++ README.md
@@ -39,3 +39,14 @@ And then
 
 
     Authorization: Bearer <YOUR_TOKEN>
+
+### Use React client
+client/.env
+
+    REACT_APP_VERIFY_API="http://localhost:8080/api/v1/verify"
+    REACT_APP_AUTH_TOKEN="<YOUR_TOKEN>"
+    REACT_APP_PAGE_TITLE="JSON Verification"
+
+cd client
+
+npm start
--- client/src/components/VerificationComponent.js
+++ client/src/components/VerificationComponent.js
@@ -38,6 +38,7 @@ const VerificationComponent = () => {
                 {
                     headers: {
                         "Content-Type": "application/json",
+                        "Authorization": "Bearer " + process.env.REACT_APP_AUTH_TOKEN,
                     },
                 }
             );
